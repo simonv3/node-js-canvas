76afe530d56f20097cfb84eded9dc130d7b0ecc2
Simon Vansintjan <svansintjan@gmail.com>
2012-09-13 16:04:36 -0700

    fixed url for online heroku

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function(){
   }
 
   // The URL of your web server (the port is set in app.js)
-  var url = 'http://localhost:5000/';
+  var url = 'http://vast-dusk-citizen.herokuapp.com/';
 
   var doc = $(document),
   win = $(window),
