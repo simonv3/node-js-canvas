b0ad6a26cdece12a1ee3417378cfd7a4081e1277
Simon Vansintjan <svansintjan@gmail.com>
2012-09-13 12:54:26 -0700

    set different ports

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var fileServer = new static.Server('./');
 
 // This is the port for our web server.
 // you will need to go to http://localhost:8080 to see it
-app.listen(8080);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 
 // If the URL of the socket server is opened in a browser
 function handler (request, response) {
