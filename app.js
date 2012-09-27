f68c59837ea1241eea9f41da7130ff0b3b6bcce7
Simon Vansintjan <svansintjan@gmail.com>
2012-09-27 14:03:50 -0700

    testing whether this works, wouldn't know why it should

--- app.js
+++ app.js
@@ -1,108 +1,36 @@
 // Including libraries
+
 var app = require('http').createServer(handler),
-  io = require('socket.io').listen(app),
-  static = require('node-static'); // for serving files
+	io = require('socket.io').listen(app),
+	static = require('node-static'); // for serving files
 
 // This will make all the files in the current folder
 // accessible from the web
 var fileServer = new static.Server('./');
-
+	
 // This is the port for our web server.
-// you will need to go to http://localhost:5000 to see it
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
+// you will need to go to http://localhost:8080 to see it
+app.listen(8080);
 
 // If the URL of the socket server is opened in a browser
 function handler (request, response) {
 
-  request.addListener('end', function () {
+	request.addListener('end', function () {
         fileServer.serve(request, response);
     });
 }
 
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
-
-
 // Delete this row if you want to see debug messages
 io.set('log level', 1);
-
-var currentID = 1;
-var lineID = 0
-var drawnLines = []
-
-// A buffer for the user's Currently drawn line
-
-var currentLine = []
-var userIDs= new Array()
 
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
 
-  socket.on('joining', function(){
-    userIDs.push(currentID)
-    console.log("user id " + currentID + " joined")
-
-    //add a currentLine for the user
-    currentLine.push([])
-    //tell everyone that someone joined
-    socket.broadcast.emit('otherUserJoined', userIDs)
-
-    //tell the user about everything else
-    socket.emit('joinedCallback', currentID, userIDs, drawnLines)
-    currentID += 1
-
-
-  })
-
-  // A mousedown means a user started drawing.
-  socket.on('mousedown', function(data){
-    //we're drawing a new line, find the user's current line
-    currentLine[data.userID-1].push(data)
-    //pass the data along to clients
-    socket.broadcast.emit("othersStartDrawing", data)
-  })
-
-  // A mouseup means the user stopped drawing. 
-  socket.on('mouseup', function(data){
-    //console.log(currentLine[data-1])
-    
-    //the user stopped drawing a line, push it to the drawnlines
-    drawnLines.push(currentLine[data-1])
-    //clear the current line buffer for the current user
-    currentLine[data-1] = []
-    //pass the data along
-    socket.broadcast.emit("othersStoppedDrawing", data)
-  })
-
-  // Start listening for mouse move events
-  socket.on('mousemove', function (data) {
-      //push the data to the current line
-    currentLine[data.userID-1].push({
-      "x":data.x,
-      "y":data.y,
-      "color":data.color,
-      "size":data.size,
-    })
-
-    //console.log(data);
-    //add to database
-    // This line sends the event (broadcasts it)
-    // to everyone except the originating client.
-    socket.broadcast.emit('othersMoving', data);
-  });
-  socket.on('testing', function(data){
-    console.log('test-data')
-    console.log(data)
-  })
-
-  //set the user ID for the next user
-
-
+	// Start listening for mouse move events
+	socket.on('mousemove', function (data) {
+		
+		// This line sends the event (broadcasts it)
+		// to everyone except the originating client.
+		socket.broadcast.emit('moving', data);
+	});
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,431 +1,120 @@
-// Code with help from Simon Sarris http://jsfiddle.net/NWBV4/10/
-
-function Pen(canvas) {
-
-    // The URL of your web server (the port is set in app.js)
-    var url = document.url;
-
-    //keeping track of how many clients there are
-    var cursors = {};
-
-    //setting up the websocket
-    var socket = io.connect(url);
-    var lastEmit = $.now();
-
-    this.myID = 0;
-
-    var tool = this;
-    var context = canvas.getContext('2d');
-    this.started = false;
-    this.color = "#333";
-    this.size = 1;
-    var move_count = 0;
-    context.strokeStyle = "#333";
-    context.lineWidth = 1;
-    context.lineJoin = 'round';
-    context.lineCap = 'round';
-    var lastx = 0;
-    var lasty = 0;
-    var width = canvas.width;
-    var height =canvas.height;
-    // create an in-memory canvas
-    var memCanvas = document.createElement('canvas');
-    memCanvas.width = width
-    memCanvas.height = height
-    var memCtx = memCanvas.getContext('2d');
-
-
-
-    //set up storage for you and others
-    this.points = []
-    this.others = []
-
-    this.otherDrawing = false
-    this.otherDrawingQueue = []
-
-
-    //tell the socket you are joining, and register your id
-
-    socket.emit("joining")
-
-    socket.on('joinedCallback', function(assignedID, userIDs, drawnLines){
-      console.log('you joined, your ID is: '+ assignedID)
-      document.getElementById("page_loader").style.display = 'none';
-      tool.myID = assignedID
-      for (var i=0 ; i < userIDs.length ; i++){
-        tool.others.push([])
-      }
-      if (drawnLines.length > 0){
-        for (var i = 0; i < drawnLines.length; i++){
-          drawPastLines(context, drawnLines[i])
-
-        //memCtx.clearRect(0, 0, width, height);
-        //memCtx.drawImage(canvas, 0, 0);
-        }
-      }
-      console.log("drawing color: " + tool.color);
-    })
-    socket.on('otherUserJoined', function(userIDs){
-      console.log('new user joined')
-      tool.others = []
-      for (i=0 ; i < userIDs.length ; i++){
-        tool.others.push([])
-      }
-    })
-
-
-    socket.on('othersStartDrawing', function(data, currentLineID){
-      if (tool.otherDrawing == false){
-        tool.others[data.id] = []
-        tool.otherDrawing = true
-        tool.others[data.id].push({
-          x: data.x,
-          y: data.y,
-          lineID: currentLineID,
-          color:data.color,
-          size:data.size,
-        })
-      } else {
-        tool.otherDrawingQueue[data.id].push({
-          x: data.x,
-          y: data.y,
-          lineID: currentLineID,
-          color:data.color,
-          size:data.size,
-        })
-      }
-    })
-
-    // the function that starts the drawing
-    function startPath(ev){
-      var initX = ev.pageX - canvas.offsetLeft
-      var initY = ev.pageY - canvas.offsetTop
-
-      //if the type of the event is touchstart, look for touches
-      if (ev.type === "touchstart"){
-        initX= ev.touches[0].pageX - canvas.offsetLeft
-        initY = ev.touches[0].pageY - canvas.offsetTop
-      }
-      tool.points.push({
-            x: initX,
-            y: initY,
-            size:tool.size,
-            color:tool.color,
-        });
-        tool.started = true;
-        socket.emit('mousedown', {
-              'x': initX,
-              'y': initY,
-              'userID': tool.myID,
-              'started':tool.started,
-              'color':tool.color,
-              'size':tool.size,
-        })
-    }
-
-
-    //treat touchstart and mousedown the same
-    this.touchstart = function(ev){ startPath(ev) };
-
-    this.mousedown = function(ev) { startPath(ev) };
-
-    //when we receive a moving message
-    socket.on('othersMoving', function(data){
-      if (tool.otherDrawing == false){
-        tool.others[data.id].push({
-          x: data.x,
-          y: data.y,
-          lineID:tool.others[data.id][0].lineID,
-          color:data.color,
-          size:tool.size,
-        })
-      } else {
-        tool.othersDrawingQueue[data.id].push({
-          x: data.x,
-          y: data.y,
-          lineID:tool.others[data.id][0].lineID,
-          color:data.color,
-          size:tool.size,
-        })
-      }
-      drawPoints(canvas, tool.others[data.id])
-    })
-
-    function movingPath(ev){
-        if (tool.started) {
-          //capture currentX and Y
-          var currentX = ev.pageX - canvas.offsetLeft
-          var currentY = ev.pageY - canvas.offsetTop
-          if (ev.type === "touchmove"){
-            currentX = ev.touches[0].pageX - canvas.offsetLeft
-            currentY = ev.touches[0].pageY - canvas.offsetTop
-          }
-          if($.now() - lastEmit > 30){
-            socket.emit('mousemove', {
-              'x': currentX + 0.5,
-              'y': currentY,
-              'userID': tool.myID,
-              'color':tool.color,
-              'size':tool.size,
-            })
-            lastEmit = $.now()
-          }
-
-            tool.points.push({
-                x: currentX,
-                y: currentY,
-                color:tool.color,
-                size:tool.size,
-            });
-            drawPoints(canvas, tool.points);
-        }
-    }
-
-    //treat touchmove and mousemove the same
-
-    this.mousemove = function(ev) { movingPath(ev) };
-
-    socket.on("othersStoppedDrawing", function(data){
-        //memCtx.clearRect(0,0, width, height);
-        //memCtx.drawImage(canvas, 0, 0);
-        tool.others[data] = [];
-        if (tool.othersDrawing == true){
-          //draw everything in the queue
-          tool.othersDrawing = false
-        }
-    })
-
-    function endPath(ev){
-        if (tool.started) {
-            tool.started = false;
-
-            // When the pen is done, save the resulting context
-            // to the in-memory canvas
-            //memCtx.clearRect(0, 0, width, height);
-            //memCtx.drawImage(canvas, 0, 0);
-            tool.points = [];
-            socket.emit('mouseup', tool.myID)
-        }
-
-
-    }
-
-    //treat touchend and mouseup as the same
-    //this.touchend = function(ev) { endPath(ev) };
-
-    this.mouseup = function(ev) { endPath(ev) };
-
-    this.changecolor = function(color){
-      context.strokeStyle=color;
-      tool.color = color;
-    }
-
-    this.setsize = function(size){
-      console.log("setting size")
-      context.strokeWidth=size;
-      tool.size = size;
-    }
-
-
-}
-
-// The general-purpose event handler. This function determines the mouse position relative to the canvas element.
-
-function ev_canvas(ev) {
-    /*if (false) {
-        ev._x = ev.touches[0].clientX;
-        ev._y = ev.touches[0].clientY; // CH: Is there a better way to do this?
-    }
-    else if (ev.layerX || ev.layerX == 0) { // Firefox
-        ev._x = ev.layerX;
-        ev._y = ev.layerY;
-    }
-    else if (ev.offsetX || ev.offsetX == 0) { // Opera
-        ev._x = ev.offsetX;
-        ev._y = ev.offsetY;
-    }
-
-    ev._x = ev._x + 0.5;
-    ev._y = ev._y + 0.5;*/
-    var func;
-    if (ev.type === "touchstart"){
-      func = PEN['mousedown']
-    } else if (ev.type === "touchmove"){
-      func = PEN['mousemove']
-    } else if (ev.type === "touchend"){
-      func = PEN['mouseup']
-    } else {
-      func = PEN[ev.type]
-    }
-    // Call appropriate event handler
-    if (func) {
-        func(ev);
-    }
-}
-
-function drawPastLines(ctx, points){
-  // move to the first point
-  ctx.beginPath()
-  ctx.strokeStyle = points[0].color;
-  ctx.lineWidth = points[0].size;
-  ctx.moveTo(points[0].x, points[0].y);
-  if (points.length == 2 ){
-    ctx.lineTo(points[1].x,points[1].y)
-  } else if (points.length == 1){
-  } else {
-
-    for (i = 1; i < points.length - 2; i ++)
-    {
-      var xc = (points[i].x + points[i + 1].x) / 2;
-      var yc = (points[i].y + points[i + 1].y) / 2;
-      
-      ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
-    }
-    ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x,points[i+1].y);
-  }
-  ctx.stroke()
-  ctx.closePath()
-
-  // curve through the last two points
-}
-
-function drawPoints(canvas, points) {
-    var ctx = canvas.getContext('2d');
-  
-    /*if (points.length < 6) {
-        var b = points[0];
-        ctx.beginPath(), ctx.arc(b.x, b.y, ctx.lineWidth / 2, 0, Math.PI * 2, !0), ctx.closePath(), ctx.fill();
-        return
-    }*/
-  ctx.beginPath()
-  ctx.strokeStyle = points[0].color
-  ctx.lineWidth = points[0].size
-  //console.log(points[0])
-  //get the slope of a the previous segment.
-  if (points.length > 3){
-
-    var i = points.length-3
-
-    //find the midpoint of the current line. 
-    var midx = (points[i+2].x+points[i+1].x)/2;
-    var midy = (points[i+2].y+points[i+1].y)/2;
-    var midPoint = {
-      "x":midx,
-      "y":midy,
-    }
-    var midm = (points[i+2].y - points[i+1].y).toFixed(4)/(points[i+2].x - points[i+1].x).toFixed(4)
-    if (midm != Infinity || midm != -Infinity){
-      perpMidM = (1/midm) * -1;
-      perpMidB = perpMidM * midx - midy;
-      //find arbitrary other point
-      var arbx = midx+1;
-      var arby = perpMidM * arbx + perpMidB;
-
-      var arbPoint = {
-        "x":arbx,
-        "y":arby,
-      }
-      var iPoint = intersectLineLine(points[i], points[i+1], midPoint, arbPoint)
-      var c = points.length-1
-      ctx.moveTo(points[c-1].x, points[c-1].y)
-
-      if (iPoint != "straight"){
-
-        drawMidX = (midx + iPoint.x)/2
-        drawMidY = (midy + iPoint.y)/2
-
-        /*ctx.fillStyle = "red";
-        ctx.lineWidth = 0;
-        ctx.beginPath();
-        ctx.arc(iPoint.x, iPoint.y, 2, 0, Math.PI*2, true); 
-        ctx.closePath();
-        ctx.fill();*/
-        ctx.moveTo(points[c-1].x, points[c-1].y)
-
-
-
-        ctx.quadraticCurveTo(drawMidX, drawMidY, points[c].x, points[c].y)
-      } else {
-        ctx.lineTo(points[c].x,points[c].y)
-      }
-      ctx.stroke()
-    }
-    ctx.closePath()
-
-  }
-
-}
-function intersectLineLine (a1, a2, b1, b2) {
-    var result;
-    var ua_t = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x);
-    var ub_t = (a2.x - a1.x) * (a1.y - b1.y) - (a2.y - a1.y) * (a1.x - b1.x);
-    var u_b = (b2.y - b1.y) * (a2.x - a1.x) - (b2.x - b1.x) * (a2.y - a1.y);
-    
-    /*if (ua_t > 10){
-      console.log("new set")
-      console.log(b1.x + " " + b1.y + "; " + b1.x + " " + b1.y);
-      console.log(a2.x + " " + a1.x)
-    }*/
-
-    if (u_b != 0 && u_b != NaN && u_b != Infinity && u_b != -Infinity && ua_t != -Infinity && ua_t != Infinity&& ub_t != -Infinity && ub_t != Infinity) {
-
-        var ua = ua_t / u_b;
-        var ub = ub_t / u_b;
-        if (Math.abs(ua) > 3 || Math.abs(ub) > 3){
-          //console.log(a1.x +" "+ a1.y + " " + ua_t + " / " + u_b)
-          result="straight"
-        } else {
-        if(ua && ub){
-        //if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
-            result = {
-              "x":a1.x + ua * (a2.x - a1.x),
-              "y":a1.y + ua * (a2.y - a1.y),
-            }
-        } else {
-            result = "straight"
-        } }
-    } else {
-        if (ua_t == 0 || ub_t == 0) {
-            result = "straight"
-        } else {
-            result = "straight"
-        }
-    }
-    return result;
-};
-
-
-setTimeout(function() {
-    // Bind canvas to listeners
-    var canvas = document.getElementById('paper');
-    PEN = new Pen(canvas);
-
-
-    canvas.addEventListener('mousedown', ev_canvas, false);
-    canvas.addEventListener('mouseout', ev_canvas, false);
-    canvas.addEventListener('touchstart', ev_canvas, false)
-    canvas.addEventListener('mousemove', ev_canvas, false);
-    canvas.addEventListener('touchmove', ev_canvas, false);
-    canvas.addEventListener('mouseup', ev_canvas, false);
-    canvas.addEventListener('touchend', ev_canvas, false);
-    $("#colors li").click( function(){
-      if (this.id =="erase"){
-        var sizefunc = PEN['setsize']
-        sizefunc(40)
-        var func = PEN['changecolor']
-        
-        func($(this).css("color"))
-        
-      } else {
-        var func = PEN['changecolor']
-        var sizefunc = PEN['setsize']
-        sizefunc(1)
-        
-        func($(this).css("color"))
-      }
-
-
-    })
-
-}, 500);
-
+$(function(){
+
+	// This demo depends on the canvas element
+	if(!('getContext' in document.createElement('canvas'))){
+		alert('Sorry, it looks like your browser does not support canvas!');
+		return false;
+	}
+
+	// The URL of your web server (the port is set in app.js)
+	var url = 'http://localhost:8080';
+
+	var doc = $(document),
+		win = $(window),
+		canvas = $('#paper'),
+		ctx = canvas[0].getContext('2d'),
+		instructions = $('#instructions');
+	
+	// Generate an unique ID
+	var id = Math.round($.now()*Math.random());
+	
+	// A flag for drawing activity
+	var drawing = false;
+
+	var clients = {};
+	var cursors = {};
+
+	var socket = io.connect(url);
+	
+	socket.on('moving', function (data) {
+		
+		if(! (data.id in clients)){
+			// a new user has come online. create a cursor for them
+			cursors[data.id] = $('<div class="cursor">').appendTo('#cursors');
+		}
+		
+		// Move the mouse pointer
+		cursors[data.id].css({
+			'left' : data.x,
+			'top' : data.y
+		});
+		
+		// Is the user drawing?
+		if(data.drawing && clients[data.id]){
+			
+			// Draw a line on the canvas. clients[data.id] holds
+			// the previous position of this user's mouse pointer
+			
+			drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+		}
+		
+		// Saving the current client state
+		clients[data.id] = data;
+		clients[data.id].updated = $.now();
+	});
+
+	var prev = {};
+	
+	canvas.on('mousedown',function(e){
+		e.preventDefault();
+		drawing = true;
+		prev.x = e.pageX;
+		prev.y = e.pageY;
+		
+		// Hide the instructions
+		instructions.fadeOut();
+	});
+	
+	doc.bind('mouseup mouseleave',function(){
+		drawing = false;
+	});
+
+	var lastEmit = $.now();
+
+	doc.on('mousemove',function(e){
+		if($.now() - lastEmit > 30){
+			socket.emit('mousemove',{
+				'x': e.pageX,
+				'y': e.pageY,
+				'drawing': drawing,
+				'id': id
+			});
+			lastEmit = $.now();
+		}
+		
+		// Draw a line for the current user's movement, as it is
+		// not received in the socket.on('moving') event above
+		
+		if(drawing){
+			
+			drawLine(prev.x, prev.y, e.pageX, e.pageY);
+			
+			prev.x = e.pageX;
+			prev.y = e.pageY;
+		}
+	});
+
+	// Remove inactive clients after 10 seconds of inactivity
+	setInterval(function(){
+		
+		for(ident in clients){
+			if($.now() - clients[ident].updated > 10000){
+				
+				// Last update was more than 10 seconds ago. 
+				// This user has probably closed the page
+				
+				cursors[ident].remove();
+				delete clients[ident];
+				delete cursors[ident];
+			}
+		}
+		
+	},10000);
+
+	function drawLine(fromx, fromy, tox, toy){
+		ctx.moveTo(fromx, fromy);
+		ctx.lineTo(tox, toy);
+		ctx.stroke();
+	}
+
+});
