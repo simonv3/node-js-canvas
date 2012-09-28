bbfecf704920f306e21c507054101d08650395c7
Simon Vansintjan <svansintjan@gmail.com>
2012-09-28 10:28:37 -0700

    made changes to also have the other people draw smoothly

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ $(function(){
     myID = assignedID;
     if (drawnLines && drawnLines.length > 0){
       for (var i = 0; i < drawnLines.length; i++){
-        if (drawnLines[i].length > 0 ){
+        if (drawnLines[i] != null && drawnLines[i].length > 0 ){
           drawPastLines(drawnLines[i]);
         }
       }
@@ -71,7 +71,7 @@ $(function(){
       // Draw a line on the canvas. clients[data.id] holds
       // the previous position of this user's mouse pointer
 
-      drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+      drawLine({"x":clients[data.id].prevx, "y":clients[data.id].prevy}, clients[data.id].x, clients[data.id].y, data.x, data.y);
     }
 
     // Saving the current client state
